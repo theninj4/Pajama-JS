cd90e319b58b02bcea0e6fab8868c0095778d8c7
Oliver Rumbelow <oliver_FF@hotmail.com>
2015-01-03 23:28:04 +0000

    Meeting todo-mvc style guide

--- website/todo-mvc/routes.js
+++ website/todo-mvc/routes.js
@@ -4,7 +4,7 @@
 	'use strict';
 
 	window.onload = function() {
-	  pjs.defineRoutes(document.body.children[0], [
+	  pjs.defineRoutes(document.querySelectorAll('body>div')[0], [
 	    { path: '*', controller: 'TodoController' },
 	  ]);
 	};
